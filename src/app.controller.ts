58a2acc515014f7c0ae82c9539b39a236aa5cbdc
paulaji <paulajiparayil123@gmail.com>
2023-08-27 19:03:58 +0530

    login controller

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,11 +1,18 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
+  // login
+  @Post('login')
+  login(): any {
+    return {};
+  }
+
+  // making this route protected such that authenticated users can only access
+  @Get('protected')
   getHello(): string {
     return this.appService.getHello();
   }
