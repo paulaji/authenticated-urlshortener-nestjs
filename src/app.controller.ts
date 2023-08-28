6d4bf39d26688166495008ec4b494fb7165b2e11
paulaji <paulajiparayil123@gmail.com>
2023-08-28 18:59:59 +0530

    login form

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,13 @@
 // working
-import { Controller, Get, Post, UseGuards, Request, Res } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  UseGuards,
+  Request,
+  Res,
+  Body,
+} from '@nestjs/common';
 import { AppService } from './app.service';
 import { LocalAuthGuard } from './auth/local-auth.guard';
 import { AuthenticatedGuard } from './auth/authenticated.guard';
@@ -8,12 +16,32 @@ import { AuthenticatedGuard } from './auth/authenticated.guard';
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  // login
+  // // test login
   // use the LocalAuthGuard for login - import it
+  // @UseGuards(LocalAuthGuard)
+  // @Post('login')
+  // login(@Request() req): any {
+  //   return { msg: 'User logged in' };
+  // }
+
+  // // working
+  // @Get('login')
+  // loginPage(@Res() res): void {
+  //   res.sendFile('login.html', { root: 'public' });
+  // }
+
+  // // test
+  @Get('login')
+  loginPage(@Res() res): void {
+    res.sendFile('login.html', { root: 'public' });
+  }
+
+  @Post('login') // Handle login form submission
   @UseGuards(LocalAuthGuard)
-  @Post('login')
-  login(@Request() req): any {
-    return { msg: 'User logged in' };
+  async login(@Request() req, @Res() res): Promise<void> {
+    // Handle successful authentication
+    // Redirect to index.html if authenticated
+    res.redirect('/index.html');
   }
 
   // making this route protected such that authenticated users can only access
