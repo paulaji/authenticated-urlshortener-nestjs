918eb70a06f79b8c365d41afa40c14d4deb1945e
paulaji <paulajiparayil123@gmail.com>
2023-08-28 12:05:07 +0530

    ukw todo

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,6 +1,7 @@
 import { Controller, Get, Post, UseGuards, Request } from '@nestjs/common';
 import { AppService } from './app.service';
 import { LocalAuthGuard } from './auth/local-auth.guard';
+import { AuthenticatedGuard } from './auth/authenticated.guard';
 
 @Controller()
 export class AppController {
@@ -15,6 +16,8 @@ export class AppController {
   }
 
   // making this route protected such that authenticated users can only access
+  // we have defined a context such that only if user is authenticated, we pass in the http requests
+  @UseGuards(AuthenticatedGuard)
   @Get('protected')
   getHello(@Request() req): string {
     return req.user;
