a719df3d6614fd0ff31800b4a404028ce426e9f9
paulaji <paulajiparayil123@gmail.com>
2023-08-28 09:06:03 +0530

    validate user

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,19 @@
 import { Injectable } from '@nestjs/common';
+import { UsersService } from 'src/users/users.service';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  // since we have imported UsersModule in auth.module.ts, we can use UsersService which we have defined
+  // creating a constructor of UsersService so that we can use it here
+  constructor(private usersService: UsersService) {}
+
+  // define a function which can validate a user by taking in the username from the injected data and checking it with it's corresponding password
+  async validateUser(username: string, password: string): Promise<any> {
+    // use the findOne function which we have defined in the UsersService
+    const user = await this.usersService.findOne(username);
+
+    if (user && user.password === password) {
+      return user;
+    }
+  }
+}
